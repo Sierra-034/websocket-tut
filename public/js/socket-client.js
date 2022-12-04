e6dc012d1b78aadda3a5dbad404b27ac7f3b276d
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-12-03 22:20:49 -0600

    216: Mensajes de conexión y desconexión - Cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,17 @@
+// Referencias de HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('Conectado');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log('Desconectado')
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+});
