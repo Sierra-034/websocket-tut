e67f568315249d13eb17b96c0566489d6bf873b2
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-12-03 22:58:25 -0600

    218: Emitir desde el servidor - Escuchar en el cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -7,15 +7,17 @@ const btnEnviar = document.querySelector('#btnEnviar')
 const socket = io();
 
 socket.on('connect', () => {
-    console.log('Conectado');
     lblOffline.style.display = 'none';
     lblOnline.style.display = '';
 });
 
 socket.on('disconnect', () => {
-    console.log('Desconectado')
     lblOnline.style.display = 'none';
     lblOffline.style.display = '';
+});
+
+socket.on('enviar-mensaje', (payload) => {
+    console.log('Desde cliente', payload);
 });
 
 btnEnviar.addEventListener('click', () => {
